69ccadd4e652a606a45ba150fea3ca75ecf7289a
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-25 14:39:14 +0200

    splitting into libraries

---
+++ code/SpecialisedStudyProject/Persistence/dataset/IDatasetLazyLoading.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Persistence.dataset
+{
+    interface IDatasetLazyLoading
+    {
+        /**
+         * 
+         */
+        public abstract string ObjectName();
+        
+        /**
+         * 
+         */
+        public abstract string ObjectType();
+
+
+        /**
+         * 
+         */
+        public abstract string[] ObjectAttributes();
+
+
+        /** 
+         *  
+         */
+        public abstract string LoadContent();
+
+        /**
+         * 
+         */
+        public abstract bool IsDone();
+    }
+}
---
+++ code/SpecialisedStudyProject/Persistence/dataset/IDatasetLoad.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Persistence.dataset
+{
+    interface IDatasetLoad
+    {
+
+        /**
+         * 
+         */
+        public abstract string ObjectName();
+
+        /**
+         * 
+         */
+        public abstract string ObjectType();
+
+
+        /**
+         * 
+         */
+        public abstract string[][] ObjectAttributes();
+        
+        /**
+         * 
+         */
+        public abstract string[] ObjectKeyAttribute( String key );
+
+
+        /** 
+         *  
+         */
+        public abstract string LoadContent();
+    }
+}
