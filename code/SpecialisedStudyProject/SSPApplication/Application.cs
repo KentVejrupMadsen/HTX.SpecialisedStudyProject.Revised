7e4d33f2889c294a850e5b9fb78d60900fabdaca
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-25 12:56:11 +0200

    adding source code and removing ide settings

---
+++ code/SpecialisedStudyProject/SSPApplication/Application.cs
@@ -0,0 +1,92 @@
+﻿/** Author: Kent vejrup Madsen
+  * Title: Application
+  * Description: 
+  * Tags: 
+  * License: MIT License 
+    * (https://raw.githubusercontent.com/KentVejrupMadsen/SpecialisedStudyProject.Revised/main/LICENSE.md)
+    * (https://opensource.org/licenses/MIT)
+  */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace SSPApplication
+{
+    internal class Application
+    {
+        // Constructors
+        public Application()
+        {
+
+        }
+
+        public Application( String[] args )
+        {
+
+        }
+
+        // Variables
+        private string[] arguments = null;
+
+        private CommandParser argumentParser = null;
+
+        // Accessors
+        public String[] Arguments
+        {
+            get
+            {
+                return this.arguments;
+            }
+
+            set
+            {
+                this.arguments = value;
+            }
+        }
+
+        public CommandParser ArgumentParser
+        {
+            get
+            {
+                if( argumentParser == null )
+                {
+                    ArgumentParser = new CommandParser();
+                }
+
+                return this.argumentParser;
+            }
+
+            set
+            {
+                this.argumentParser = value;
+            }
+        }
+
+        // Code
+        public void initialise()
+        {
+
+        }
+
+        public void execute()
+        {
+
+        }
+
+        public void gc()
+        {
+
+        }
+
+        // Execution
+        public void run()
+        {
+            this.initialise();
+            this.execute();
+            this.gc();
+        }
+    }
+}
---
+++ code/SpecialisedStudyProject/SSPApplication/Program.cs
@@ -0,0 +1,19 @@
+﻿/** Author: Kent vejrup Madsen
+  * Title: Program
+  * Description: 
+  * Tags: 
+  * License: MIT License 
+    * (https://raw.githubusercontent.com/KentVejrupMadsen/SpecialisedStudyProject.Revised/main/LICENSE.md)
+    * (https://opensource.org/licenses/MIT)
+  */
+namespace SSPApplication
+{
+    internal class Program
+    {
+        public static void Main( String[] arguments )
+        {
+            Application app = new Application( arguments );
+            app.run();
+        }
+    }
+}
