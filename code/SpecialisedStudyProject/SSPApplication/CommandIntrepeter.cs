0e7db9394ea3102e3c98c8179a163857da8f080f
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-25 13:26:57 +0200

    working on project

---
+++ code/SpecialisedStudyProject/SSPApplication/CommandIntrepeter.cs
@@ -0,0 +1,26 @@
+﻿/** Author: Kent vejrup Madsen
+  * Title: CommandIntrepeter
+  * Description: 
+  * Tags: 
+  * License: MIT License 
+    * (https://raw.githubusercontent.com/KentVejrupMadsen/SpecialisedStudyProject.Revised/main/LICENSE.md)
+    * (https://opensource.org/licenses/MIT)
+  */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSPApplication
+{
+    internal class CommandIntrepeter
+    {
+        public CommandIntrepeter()
+        {
+
+        }
+
+
+    }
+}
