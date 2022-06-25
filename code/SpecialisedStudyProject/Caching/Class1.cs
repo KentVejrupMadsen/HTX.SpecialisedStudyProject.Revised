dab42c41b64c4d79b68db664befa210985b082b8
Kent vejrup Madsen <Kent.vejrup.Madsen@outlook.com>
2022-06-25 14:40:50 +0200

    adding header

--- code/SpecialisedStudyProject/Caching/Class1.cs
+++ code/SpecialisedStudyProject/Caching/Class1.cs
@@ -1,4 +1,12 @@
-﻿namespace Caching
+﻿/** Author: Kent vejrup Madsen
+  * Title: InteractiveConsole
+  * Description: 
+  * Tags: 
+  * License: MIT License 
+    * (https://raw.githubusercontent.com/KentVejrupMadsen/SpecialisedStudyProject.Revised/main/LICENSE.md)
+    * (https://opensource.org/licenses/MIT)
+  */
+namespace Caching
 {
     public class Class1
     {
--- code/SpecialisedStudyProject/Persistence/ConfigurationFileSystem.cs
+++ code/SpecialisedStudyProject/Persistence/ConfigurationFileSystem.cs
@@ -1,4 +1,12 @@
-﻿using System;
+﻿/** Author: Kent vejrup Madsen
+  * Title: InteractiveConsole
+  * Description: 
+  * Tags: 
+  * License: MIT License 
+    * (https://raw.githubusercontent.com/KentVejrupMadsen/SpecialisedStudyProject.Revised/main/LICENSE.md)
+    * (https://opensource.org/licenses/MIT)
+  */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
